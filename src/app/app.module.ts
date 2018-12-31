c67aca8d553e2d3f64fa004b364bceb35d62e736
Aristid <aristidjakaj3@gmail.com>
2018-12-30 14:30:30 -1100

    Angular#2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -8,19 +7,21 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { AppComponent } from './app.component';
 
 import 'hammerjs';
-
-import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
@@ -30,22 +31,26 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
-    AboutComponent,
     HomeComponent,
+    AboutComponent,
     ContactComponent
   ],
   imports: [
     BrowserModule,
-    MatListModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
+    AppRoutingModule,
     MatToolbarModule,
+    MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule,
-    FlexLayoutModule,
-    AppRoutingModule
+    MatButtonModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
